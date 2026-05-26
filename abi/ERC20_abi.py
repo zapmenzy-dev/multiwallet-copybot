536ed66dd34703f07d753f60bd729390d6d47bc2
zapmenzy-dev <zapmenzy@gmail.com>
2026-05-26 20:58:20 +0300

    Add files via upload

---
+++ abi/ERC20_abi.py
@@ -0,0 +1,9 @@
+ERC20_ABI = [
+    {
+        "constant": True,
+        "inputs": [{"name": "_owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "balance", "type": "uint256"}],
+        "type": "function",
+    }
+]
