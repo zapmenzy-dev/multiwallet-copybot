dad8a37a074698f85da2bb1d60b871db7d700710
zapmenzy-dev <zapmenzy@gmail.com>
2026-05-26 22:51:23 +0300

    Delete abi directory

--- abi/ERC20_abi.py
+++
@@ -1,9 +0,0 @@
-ERC20_ABI = [
-    {
-        "constant": True,
-        "inputs": [{"name": "_owner", "type": "address"}],
-        "name": "balanceOf",
-        "outputs": [{"name": "balance", "type": "uint256"}],
-        "type": "function",
-    }
-]
